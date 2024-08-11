7892ec37cb7b5aa42816933d8ef93f6ce8691f8e
dabadahiburi <yuuki11250310495@gmail.com>
2024-08-12 00:37:33 +0900

    Update CORS settings

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'  # または特定のドメイン 'https://your-frontend-domain.com'
+    origins ENV['ALLOWED_ORIGIN'] || '*'
+    
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
