f878334faea05a5a1a452b8bd59a1faa60bc7b4d
dabadahiburi <yuuki11250310495@gmail.com>
2024-08-11 11:12:06 +0900

    rails API設定完了

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,41 @@
+class TodosController < ApplicationController
+  before_action :set_todo, only: [:show,:update,:destroy]
+
+  #Get /todos
+  def index
+    @todos = Todo.all
+    render json: @todos
+  end
+
+  #Get /todos/:id
+  def show
+    render json: @todo
+  end
+
+  #POST /todos
+  def create
+    @todo = Todo.new(todo_params)
+
+    if @todo.save
+      render json: @todo, status: :created,location: @todo
+    else
+      render json: @todo.errors,status: :unprocessable_entity
+    end
+  end
+
+  # Delete /todos/:id
+  def destroy
+    @todo.destroy
+  end
+
+  private
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+
+  def todo_params
+    params.require(:todo).permit(:title,:description,:completed)
+  end
+
+end
